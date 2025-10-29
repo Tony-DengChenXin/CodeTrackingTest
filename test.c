4b7a579e190d7e1807225b11bdb7a75f309eda7d
dengchenxin <547054140@qq.com>
2025-10-29 14:39:17 +0800

    feat：修改test

--- test.c
+++ test.c
@@ -2,20 +2,7 @@
 #include <stdlib.h> 
 
 
-int main(int argc, char *argv[]) {
-    
-    if (argc < 3) {
-        printf("Usage: %s <number1> <number2>\n", argv[0]);
-        return 1; 
-    }
-
-    
-    int a = atoi(argv[1]);     
-    int b = atoi(argv[2]);     
-    int sum = a + b;
-
-    
-    printf("The sum of %d and %d is: %d\n", a, b, sum);
-    
+int main() {
+    printf("Hello, World!\n");
     return 0;
 }
