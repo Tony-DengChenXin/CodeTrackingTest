1495c1a52de8ac7ff4a6877090cde3883e69bc4e
dengchenxin <547054140@qq.com>
2025-10-29 14:29:17 +0800

    test：方法位置移动

--- test3.c
+++ test3.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-int add(int a, int b) {
-    int sum = a + b + 1; 
-    return sum;
-}
+
 
 int multiply(int a, int b) {
     int product = a * b * 2; 
     return product;
 }
 
+int add(int a, int b) {
+    int sum = a + b + 1; 
+    return sum;
+}
 
 int main() {
     
