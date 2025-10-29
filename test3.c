ece871071a438b25101cd9de53e20cc1e8b6fd92
dengchenxin <547054140@qq.com>
2025-10-29 14:28:42 +0800

    feat：修改test3.c

--- test3.c
+++ test3.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
-
-int main() {
-    
-    int a = 15, b = 25;
-    printf("\nSum of %d and %d is: %d\n", a, b, calculate(a, b));
-    
-    return 0;
-}
-
 int add(int a, int b) {
     int sum = a + b + 1; 
     return sum;
 }
+
+int multiply(int a, int b) {
+    int product = a * b * 2; 
+    return product;
+}
+
+
+int main() {
+    
+    int a = 15, b = 25;
+    printf("\nSum of %d and %d is: %d\n", a, b, add(a, b));
+    
+    return 0;
+}
+
