e3a31c27a1ceb01a8231b959f9d56a0cd9b0a193
dengchenxin <547054140@qq.com>
2025-10-29 09:47:32 +0800

    test：方法签名，方法体发生改变(main)

--- test.c
+++ test.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
-int main() {
-    int a = 5;          
-    int b = 10;         
-    int sum = a + b;    
+#include <stdlib.h> 
+
+
+int main(int argc, char *argv[]) {
     
-    printf("Sum: %d\n", sum);  
+    if (argc < 3) {
+        printf("Usage: %s <number1> <number2>\n", argv[0]);
+        return 1; 
+    }
+
+    
+    int a = atoi(argv[1]);     
+    int b = atoi(argv[2]);     
+    int sum = a + b;
+
+    
+    printf("The sum of %d and %d is: %d\n", a, b, sum);
+    
     return 0;
 }
