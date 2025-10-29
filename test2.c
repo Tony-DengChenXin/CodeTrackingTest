a5a14534535a46c7eae6bb7947d792ead7096c31
dengchenxin <547054140@qq.com>
2025-10-29 10:01:27 +0800

    test：方法签名，方法体发生改变(非main)

--- test2.c
+++ test2.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 
-int add(int num1, int num2) {
-    return num1 + num2;
+
+int calculate(int num1, int num2, char operation) {
+    switch(operation) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            if(num2 != 0) return num1 / num2;
+            else return 0; 
+        default:
+            return num1 + num2; 
+    }
 }
 
-
 int main() {
-    
-    
     int a = 15, b = 25;
-    printf("\nSum of %d and %d is: %d\n", a, b, add(a, b));
+    
+    
+    printf("\nSum of %d and %d is: %d\n", a, b, calculate(a, b, '+'));
+    printf("Difference of %d and %d is: %d\n", a, b, calculate(a, b, '-'));
+    printf("Product of %d and %d is: %d\n", a, b, calculate(a, b, '*'));
     
     return 0;
 }
